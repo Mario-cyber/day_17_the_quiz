10a63c2c722e278862e58f0f1059ac0a8f7b6dde
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-03-05 02:22:17 +0000

    creating and setting attributes into a class

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 class User : 
-    pass
-
-
-user_1 = User()
+    #the innit function runs anytime an object is created from this class 
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        
+user_1 = User("001", "mario")
+user_1.user_id = "002"
 
 print(user_1)
+print(user_1.user_id)
