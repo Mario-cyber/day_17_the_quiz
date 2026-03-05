65a15a050aad7d471c7cf72b330579ac8bfaf4f7
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-03-05 23:40:50 +0000

    QuizBrain class created

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
+
 
 question_bank = []
 
@@ -7,3 +9,6 @@ for question in question_data:
     new_question = Question(question["text"],question["answer"])
     question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
+
+quiz.next_question()  
